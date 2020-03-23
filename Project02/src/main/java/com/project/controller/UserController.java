8afa947ef498dd52177d86f9588cc1448d09f11d
Yong Liang <yongliang24@gmail.com>
2020-03-22 22:51:08 -0400

    created controller for user (#2)

---
+++ Project02/src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.project.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.model.User;
+import com.project.repository.UserRepository;
+
+@RestController 
+@CrossOrigin(origins = "*")	
+@RequestMapping("/api/v1")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userRepo.findAll();
+	}
+	
+	@PostMapping("/users")
+	public User createUser(@Valid @RequestBody User user) {
+		return userRepo.save(user);
+	}
+	
+	
+}
