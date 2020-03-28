7e57cae2d38a311e7f2688f0b473ebd82f24e216
‘yongliang24’ <yongliang24@gmail.com>
2020-03-28 01:09:05 -0400

    fixed db

--- Project02/src/main/java/com/project/Project02Application.java
+++ Project02/src/main/java/com/project/Project02Application.java
@@ -3,6 +3,7 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
