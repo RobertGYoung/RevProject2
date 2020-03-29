b41734c97594913e57a3bf305275cf7d2d84c6db
Ezra <zradudden@gmail.com>
2020-03-29 16:29:09 -0400

    Deleting Likes backend completion

--- Project02/src/main/java/com/project/controller/UserController.java
+++ Project02/src/main/java/com/project/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.project.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.model.Like;
 import com.project.model.User;
+import com.project.repository.LikeRepository;
 import com.project.repository.UserRepository;
 import com.project.exception.ResourceNotFoundException;
 
@@ -32,6 +36,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepo;
+	
+	@Autowired
+	private LikeRepository likeRepo;
 	
 	@PostConstruct
 	public void initDatabaseValues() {
@@ -65,14 +72,26 @@ public class UserController {
         return ResponseEntity.ok().body(user);
 
 	}
+	
+	@DeleteMapping("/users/{id}/likes")
+	public ResponseEntity<User> deleteLikeByUser(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException{
+		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		likeRepo.deleteByUser(user);
+		Set<Like> likes= user.getLikes();
+		likes.clear();
+		user.setLikes(likes);
+		final User updatedUser = userRepo.saveAndFlush(user);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
 	// update user
-		@PatchMapping("/users/{id}")
-		public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetail)
-				throws ResourceNotFoundException {
-			User user = userRepo.findById(userId)
-					.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
-			user.setPassword(userDetail.getPassword());
-			final User updatedUser = userRepo.save(user);
-			return ResponseEntity.ok(updatedUser);
-		}
+	@PatchMapping("/users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User userDetail)
+			throws ResourceNotFoundException {
+		User user = userRepo.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		user.setPassword(userDetail.getPassword());
+		final User updatedUser = userRepo.save(user);
+		return ResponseEntity.ok(updatedUser);
+	}
 }
